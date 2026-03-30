cefcd0bdbb731394e1a474927e10d93178f90200
Horacio8819 <horace.djousse@yahoo.com>
2026-03-30 14:11:07 +0200

    Work in progress / prepare for new version

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -7,3 +7,13 @@ output "asg_name" {
   value       = aws_autoscaling_group.app_asg.name
   description = "The name of the Auto Scaling Group"
 }
+
+output "alb_security_group_id" {
+  description = "The security group ID of the ALB"
+  value       = aws_security_group.alb_sg.id
+}
+
+output "instance_security_group_id" {
+  description = "The security group ID of the EC2 instances"
+  value       = aws_security_group.ec2_sg.id
+}
